61bcda2d5d06314445711fa85ef94b41e9608795
KitKat-ru <althome195@gmail.com>
2022-06-30 23:31:57 +0300

    deploy_sprint_docker3

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -62,17 +62,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-
 # Database
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default ='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default = 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', default = ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default = ''),
-        'HOST': os.getenv('DB_HOST', default = 'db'),
-        'PORT': os.getenv('DB_PORT', default = 5432)
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default=''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
 # Password validation
